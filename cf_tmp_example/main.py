138d9f14c90e3ed729aa51771452fb5218aa4f32
HPOLESE1 <hpolese1@ford.com>
2021-08-03 23:46:20 -0300

    Creating files on /tmp folder succesfully. Writing to GCS succeeds but its inherting the path as well.

--- cf_tmp_example/main.py
+++ cf_tmp_example/main.py
@@ -9,35 +9,32 @@ GITHUB_TOKEN = ''
 #creds_path = r"C:\Users\ABSOLUTE_PATH_TO_YOUR_SEVICE_ACCOUNT_CREDENTIALS\gcp_creds.json"
 #environ['GOOGLE_APPLICATION_CREDENTIALS'] = creds_path
 
-# -- Cloud Storage Client --
+# Cloud Storage Client
 storage_client = storage.Client()
 bucket_name = 'cf_tmp_bucket'
 bucket = storage_client.get_bucket(bucket_name) 
 
-# root path on CF will be /workspace
+# Root path on CF will be /workspace, while on local Windows: C:\
 root = path.dirname(path.abspath(__file__))
 
 def parse_from_git(request):
-    path = '.'
+    repo_path = '.'
     branch = 'master'
     repository_url = 'hpoleselo/gcs_test'
     git = Github(GITHUB_TOKEN)
     repo = git.get_repo(repository_url)
-    repo_files = repo.get_contents(path)
-
-    #with ZipFile('zipped_files.zip', 'w') as zipObject:
-    with ZipFile(path.join(root, '/tmp/zipped_files.zip'), 'w') as zipObject:
+    repo_files = repo.get_contents(repo_path)
+    zipped_files_path = '/tmp/zipped_files.zip'
+    with ZipFile(path.join(root, zipped_files_path), 'w') as zipObject:
         for file in repo_files:
             content = get_content_from_git(file)
-            print(f'BORA $$$ {file.name}')
-            zip_path = '/tmp' + file.name
-            zip_path = path.join(root, zip_path)
+            zip_path = '/tmp/' + file.name
+            zip_path_f = path.join(root, zip_path)
             file_local = create_file(file.name, content)
-            zipObject.write(zip_path, arcname=file.name)
-            #zipObject.write(file.name, arcname=file.name)
+            zipObject.write(zip_path_f, arcname=file.name)
 
-    write_to_gcs(path.join(root, '/tmp/zipped_files.zip'))
-    #write_to_gcs('zipped_files.zip')
+    # ! When writing to GCS, C:\ is coming as well.
+    write_to_gcs(path.join(root, zipped_files_path))
 
     return 'Files written to GCS', 200
 
@@ -46,11 +43,11 @@ def get_content_from_git(file):
     return content
 
 def create_file(file_name, file_content):
-    file_path = '/tmp' + file_name
+    file_path = '/tmp/' + file_name
     file_path = path.join(root, file_path)
 
     # If file is a binary, we rather use 'wb' instead of 'w'
-    with open(file_name, 'w') as file:
+    with open(file_path, 'w') as file:
         file.write(file_content)
 
 def write_to_gcs(file):
